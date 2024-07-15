d00b26a1c93ff27eae978115ba8927b31ce4bd0a
Gabriella Massaro <gmassaro807@gmail.com>
2024-07-14 22:14:01 -0400

    All tests pass

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.map do |attendee|
+    "Hello, my name is #{attendee}."
+  end
+end
+
+def assign_rooms(room_assignments)
+  room_assignments.each_with_index.map do |name, index|
+    "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+  end
+end
+
+def printer(badges_and_room_assignments)
+  badges_and_room_assignments.map do |name|
+    puts "Hello, my name is #{name}."
+  end
+  badges_and_room_assignments.each_with_index.map do |name, index|
+    puts "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+  end
+end
